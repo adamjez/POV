7c3c8aa8ca5a50dcea4845702fa1ccf616519e54
adamjez <adamjez@outlook.cz>
2016-12-12 16:43:14 +0100

    Adding some feet detection

--- POV/process_players.py
+++ POV/process_players.py
@@ -2,6 +2,7 @@ import sys
 import cv2
 import numpy as np
 from scipy.ndimage.filters import gaussian_filter
+import matplotlib.pyplot as plt
 
 import models
 
@@ -18,6 +19,7 @@ class ProcessPlayers:
         self.playersCount = playersCount
         self.distanceBetweenDummys = distanceBetweenDummys
         self.dummyHeight = 40
+        self.stripWidth = 75
 
     def detect(self, image):
         height, width, channels = image.shape
@@ -37,65 +39,61 @@ class ProcessPlayers:
 
             dummyStrips = []
             for index in dummyIndexes:
-                strip = image[index - 5: index + 5, linePos - 60:linePos + 60].copy()
+                cv2.rectangle(image, (linePos - self.stripWidth, index - 5), (linePos + self.stripWidth, index + 5), (255, 0, 0))
+
+                strip = image[index - 5: index + 5, linePos - self.stripWidth:linePos + self.stripWidth].copy()
                 dummyStrips.append(strip)
                 cv2.circle(image, (linePos, index), 5, (255, 0, 0), 10)
                 #cv2.imshow("test", strip)
                 #cv2.waitKey()
+            #dummyStrips[0] = cv2.cvtColor(dummyStrips[0], cv2.COLOR_RGB2GRAY)
 
             (width, center) = self.computeDummyWidth(dummyStrips, currentPlayerColor)
             for index in dummyIndexes:
-                dummys.append(models.Dummy((linePos, index), belongs, lineIndex))
+                dummys.append(models.Dummy((linePos, index), belongs, lineIndex, (linePos + center, index)))
                 cv2.rectangle(image, (linePos + center - int(width/2), index - int(self.dummyHeight / 2)), (linePos + center + int(width/2), index + int(self.dummyHeight / 2)), (255, 0, 0))
 
             #cv2.imshow("Image", image)
             #cv2.waitKey()
             lineIndex += 1
 
-        #cv2.imshow("Image", image)
-        #cv2.waitKey()
-
         return dummys
 
     def computeDummyWidth(self, strips, playerColor):
         colorValues = []
-        color = (99, 44)
-        strips[0] = cv2.cvtColor(strips[0], cv2.COLOR_RGB2HSV)
-        strips[1] = cv2.cvtColor(strips[1], cv2.COLOR_RGB2HSV)
-        playerColor = playerColor if playerColor == self.player1Color else (110,  90, 161)
-        #playerColor = (120, 242, 140)
-        #cv2.imshow("test1", strips[0])
-        #cv2.imshow("test2", strips[1])
-        #cv2.waitKey()
-        for i in range(120):
+        i = 0
+        for strip in strips:
+            gray = cv2.cvtColor(strip,cv2.COLOR_RGB2GRAY)
+            ret,gray = cv2.threshold(gray,162,255,0)
+            strips[i] = gray
+            #strips[i] = cv2.cvtColor(strip, cv2.COLOR_RGB2HSV)
+            i += 1
+
+        playerColor = 255                         
+
+        for i in range(self.stripWidth * 2):
             colorDiff = 0
             for j in range(10):
-                colorDiff += abs(strips[0][j, i][1] - playerColor[1]) + abs(strips[1][j, i][1] - playerColor[1]) 
-                + abs(strips[0][j, i][0] - playerColor[0]) + abs(strips[1][j, i][0] - playerColor[0])
-                + (abs(strips[0][j, i][2] - playerColor[2]) + abs(strips[1][j, i][2] - playerColor[2])) / 2
-            colorValues.append(colorDiff)
+                for strip in strips:
+                    colorDiff += abs(strip[j, i] - 255)
+            colorValues.append(int(colorDiff))
 
-        colorValues = colorValues / np.max(colorValues)
-        colorValues = gaussian_filter(colorValues, sigma=2, mode='nearest')
+        colorValues = gaussian_filter(colorValues, sigma=1)
 
-        #plt.plot(range(0, len(colorValues)), colorValues)
+        #colorValues = colorValues / np.max(colorValues)
 
-        index = colorValues.argmin(axis=0)
-        colorValues[self.normalize(index - 16, 120):self.normalize(index + 16, 120)] = sys.maxsize
-        index2 = colorValues.argmin(axis=0)
         #plt.plot(range(0, len(colorValues)), colorValues)
         #plt.show()
 
-        if colorValues[index2] < 0.4:
-            # we got two indexes
-            mainIndex = index if abs(index - 60) < abs(index2 - 60) else index2
-            minorIndex = index2 if mainIndex == index else index
+        index = self.stripWidth
+        i = 0
+        for x in colorValues:
+            # Magic constnat? Yes, change it if detection of feet doesn't work, but be carefoul it can damage your computer
+            if x < 3000 and abs(i - self.stripWidth) >abs(index - self.stripWidth):
+                index = i
+            i += 1
 
-            center = int((minorIndex - 60) * 1.3)
-            width = 40
-            return (width, center)
-
-        return (self.linesWidth, 0)
+        return (self.linesWidth, index - self.stripWidth)
 
     def segmentLines(self, image, color, playersCount, computeLastMiddlePlayer):
         rows = self.compueMeanSquareForEachRow(cv2.cvtColor(image, cv2.COLOR_RGB2HSV), color)
